e7c9d6c6aed01d7db8c768c2d1fbe680e8a67c39
Brandon Bakus <brandonbakus@gmail.com>
2025-01-04 15:37:36 -0500

    Yay Math

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+
+let num1 = 2
+let num2 = 31
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 5) + 2
+
+
+let num3 = 9
+let num4 = 5
+const mod = num3 % num4
+
+
+const max = Math.max(1,5,10,20)
